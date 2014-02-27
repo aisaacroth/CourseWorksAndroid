5732e7b081bec52e6b1293b2ac1f0b1b039b85ee
aisaacroth <aisaacroth@gmail.com>
2014-02-27 13:02:14 -0500

    "Started working on the CasClient for CAS Authentication."

--- Courseworks/src/com/aisaacroth/courseworks/CasClient.java
+++ Courseworks/src/com/aisaacroth/courseworks/CasClient.java
@@ -1,5 +1,15 @@
 package com.aisaacroth.courseworks;
 
-public class CasClient {
+import org.jasig.cas.util.HttpClient;
+import org.jasig.cas.web.support.CasArgumentExtractor;
 
+public class CasClient {
+	private static final String TAG = "CASCLIENT";
+	private static final String CAS_LOGIN_URL_PART = "login";
+	private static final String CAS_LOGOUT_URL_PART = "logout";
+	private static final String CAS_SERVICE_VALIDATE_URL_PART = "serviceValidate";
+	private static final String CAS_TICKET_BEGIN = "ticket=";
+	private static final String CAS_LT_BEGIN = "name=\"lt\" value=\"";
+	private static final String CAS_USER_BEGIN = "<cas:user>";
+	private static final String CAS_USER_END = "</cas:user>";
 }
