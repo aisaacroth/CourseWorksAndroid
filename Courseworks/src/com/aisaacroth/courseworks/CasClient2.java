7db1d5e4ec9fea890fa7307d6f44dc325c1558f5
aisaacroth <aisaacroth@gmail.com>
2014-03-27 11:45:23 -0400

    I'm going to attempt to fake out the CAS server by using Jsoup to gather all the necessary information and bundle it together in a POST to the CAS servers.

--- Courseworks/src/com/aisaacroth/courseworks/CasClient2.java
+++ Courseworks/src/com/aisaacroth/courseworks/CasClient2.java
@@ -2,34 +2,29 @@ package com.aisaacroth.courseworks;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
 import java.net.CookieHandler;
 import java.net.CookieManager;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
 
 import javax.net.ssl.HttpsURLConnection;
 
-import org.apache.http.NameValuePair;
 import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.cookie.Cookie;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.util.Log;
 
-//import android.util.Log;
-
-/*******************************************************************************
+/**
  * A second attempt at implementing a CAS client between the CAS Server and the
  * android application.
  * 
  * @author Alexander Roth
  * @data 2014-03-04
  * @version 0.1
- ******************************************************************************/
+ **/
+@TargetApi(Build.VERSION_CODES.GINGERBREAD)
 public class CasClient2 {
 
 	private static final String TAG = "CASCLIENT2";
@@ -42,27 +37,29 @@ public class CasClient2 {
 	private static final String CAS_USER_END = "</cas:user>";
 	private String casBaseUrl;
 	private HttpsURLConnection conn;
-
-	/***************************************************************************
+	private final String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
+    private List<String> cookies;
+	
+	/**
 	 * Instantiates a new cas client2.
 	 * 
 	 * @param httpClient
 	 *            the http client
 	 * @param casBaseURL
 	 *            the cas base url
-	 **************************************************************************/
+	 **/
 	public CasClient2(String casBaseURL) {
 		this.casBaseUrl = casBaseURL;
 	}
 
-	/***************************************************************************
+	/**
 	 * Login.
 	 * 
 	 * @param serviceUrl
 	 *            the service url
 	 * @param credentials
 	 *            the credentials
-	 **************************************************************************/
+	 **/
 	public String login(String serviceUrl,
 			UsernamePasswordCredentials credentials) throws IOException {
 
@@ -81,19 +78,58 @@ public class CasClient2 {
 		String page = this.getPage(this.casBaseUrl + CAS_LOGIN_URL_PART
 				+ "?service=" + serviceUrl);
 
-		InputStream is = conn.getInputStream();
-		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-		String line = null;
-		while ((line = reader.readLine()) != null) {
-			System.out.println("line: " + line);
-		}
-
 		String serviceTicket = null;
 		return serviceTicket;
 	}
 
-	private String getPage(String url) throws MalformedURLException {
+	private String getPage(String url) throws IOException {
 		URL temp = new URL(url);
+		conn = (HttpsURLConnection) temp.openConnection();
+
+		conn.setRequestMethod("GET");
+		conn.setUseCaches(false);
+
+		// act like a browser
+		conn.setRequestProperty("User-Agent", USER_AGENT);
+		conn.setRequestProperty("Accept",
+				"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+		conn.setRequestProperty("Accept-Language", "en-US,en;q=0.8");
+		
+		if (cookies != null) {
+			for (String cookie : this.cookies) {
+				conn.addRequestProperty("Cookie", cookie.split(";", 1)[0]);
+			}
+		}
+		
+		int responseCode = conn.getResponseCode();
+		Log.d(TAG, "Sending 'GET' request to URL : " + temp);
+		Log.d(TAG, "Response Code: " + responseCode);
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+		
+		// Get the response cookies
+		setCookies(conn.getHeaderFields().get("Set-Cookie"));
+		
+		return response.toString();
+	}
+	
+	public String getFormParams(String html, String username, String password) {
+		Log.d(TAG, "Extracting form's data...");
 		return null;
+	}
+	
+	public List<String> getCookies(){
+		return cookies;
+	}
+	
+	public void setCookies(List<String> cookies) {
+		this.cookies = cookies;
 	}
 }
