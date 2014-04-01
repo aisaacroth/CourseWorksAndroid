96cbc6e53579b0e7f7dcaf84cb093ba8424a5d0f
aisaacroth <aisaacroth@gmail.com>
2014-04-01 11:20:41 -0400

    The Oauth Client class

---
+++ Courseworks/src/com/aisaacroth/courseworks/OAuthClient.java
@@ -0,0 +1,16 @@
+package com.aisaacroth.courseworks;
+
+
+
+/**
+ * An Oauth Client for the Courseworks Android application. Interacts with the 
+ * web version of Courseworks and accepts user-based information. Performs 
+ * authentication based on Oauth specifications.
+ * 
+ * @author  Alexander Roth
+ * @date    2014-04-01
+ * @version 0.1
+ */
+public class OAuthClient {
+
+}
