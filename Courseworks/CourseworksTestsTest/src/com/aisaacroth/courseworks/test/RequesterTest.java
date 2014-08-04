8c3ab013775a847c8f4967b203f477fcf5557b77
aisaacroth <aisaacroth@gmail.com>
2014-08-04 09:37:33 -0400

    Cleaned up the RequesterTest and removed the CookieParser from the system. It was not necessary since we found another method to parse cookies.

--- Courseworks/CourseworksTestsTest/src/com/aisaacroth/courseworks/test/RequesterTest.java
+++ Courseworks/CourseworksTestsTest/src/com/aisaacroth/courseworks/test/RequesterTest.java
@@ -2,9 +2,7 @@ package com.aisaacroth.courseworks.test;
 
 import java.io.IOException;
 
-import com.aisaacroth.courseworks.requesters.Requester;
 import com.aisaacroth.courseworks.utils.CASAuthUtil;
-import com.aisaacroth.courseworks.utils.CookieParser;
 
 import junit.framework.TestCase;
 
@@ -22,15 +20,6 @@ public class RequesterTest extends TestCase {
         username = "air2112";
         password = "BA115hp34";
         grantingTicket = CASAuthUtil.getGrantingTicket(username, password);
-    }
-
-    public void testGrabJsessionCookie() throws IOException {
-        String serviceTicket = null;
-        serviceTicket = CASAuthUtil.login(username, grantingTicket);
-        String url = "https://sakaidev.cc.columbia.edu/sakai-login-tool/container?ticket="
-                + serviceTicket;
-        String cookie = CookieParser.getCookie(url, "Set-Cookie");
-        assertTrue(cookie.contains("JSESSION"));
     }
 
     protected void tearDown() throws Exception {
